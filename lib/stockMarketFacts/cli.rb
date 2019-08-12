cc212afe3d94f007d93e5a5a210cdf8ea40dba10
Corey H <c.l.halicke@gmail.com>
2019-08-12 20:46:42 +0000

    modified: cli.rb

--- lib/stockMarketFacts/cli.rb
+++ lib/stockMarketFacts/cli.rb
@@ -5,22 +5,26 @@ class StockMarketFacts::CLI
     main_menu
   end
 
+  def list_menu
+    puts <<-DOC
+    What would you like to view?
+    1. Most Popular Stocks
+    2. Key Stats
+    3. World Markets
+    4. Gainers
+    5. Losers
+    6. Sector Performance
+    7. Commodities
+    8. How stocks are doing this year
+    9. Search by Stock Symbol
+    'x' to exit
+    DOC
+  end
+
   def main_menu
   input = nil
+  list_menu
     while input != 'x'
-      puts <<-DOC
-      What would you like to view?
-        1. Most Popular Stocks
-        2. Key Stats
-        3. World Markets
-        4. Gainers
-        5. Losers
-        6. Sector Performance
-        7. Commodities
-        8. How stocks are doing this year
-        9. Search by Stock Symbol
-        'x' to exit
-      DOC
       input = gets.strip.downcase
       case input
       when "1"
@@ -42,8 +46,15 @@ class StockMarketFacts::CLI
       when "9"
         puts "Search by Stock Symbol"
         puts "Type in the Stock Symbol for the Company you are looking for:"
+      else
+        puts "Not sure what you would like to do. Please try again."
       end
     end
   end
 
 end
+
+=begin
+git add --all
+git commit -m "modified: "
+=end
