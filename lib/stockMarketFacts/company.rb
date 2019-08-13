b842f3addeaf40414dee47bcb5274b3747e9805e
Corey H <c.l.halicke@gmail.com>
2019-08-13 02:43:33 +0000

    modified: company.rb

--- lib/stockMarketFacts/company.rb
+++ lib/stockMarketFacts/company.rb
@@ -1,17 +1,27 @@
-class StockMarketFacts::Market
+class StockMarketFacts::Company
 
-  attr_accessor :popular_stocks, :key_stats, :world_market, :gainers, :losers, :sector_performance, :commodities, :ytd_stock_performance
+  attr_accessor :simple_performace, :today_trading, :growth, :competitors, :financials, :profile_info, :company_description, :company_contact, :shareholders, :top_executives
 
-  def initialize
-    @popular_stocks = []
-    @key_stats = []
-    @world_market = []
-    @gainers = []
-    @losers = []
-    @sector_performance = []
-    @commodities = []
-    @ytd_stock_performance = []
-    self.fill_data
+  def initialize(company_symbol)
+    @simple_performace
+    @today_trading
+    @growth
+    @competitors
+    @financials
+    @profile_info
+    @company_description
+    @company_contact
+    @shareholders
+    @top_executives
+    self.fill_data(company_symbol)
+  end
+
+  def fill_data(company_symbol)
+    quote_url = "https://money.cnn.com/quote/quote.html?symb=#{company_symbol}"
+    profile_url = "https://money.cnn.com/quote/profile/profile.html?symb=#{company_symbol}"
+    quote_doc = Nokogiri::HTML(open(quote_url))
+    profile_doc = Nokogiri::HTML(open(profile_url))
+    binding.pry
   end
 
 
